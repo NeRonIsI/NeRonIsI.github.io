2d1c14af26fb846f3a703fa31a627309fe0df785
NeRonIsI <fragv2@mail.ru>
2017-05-18 22:03:02 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var link = document.querySelector(".booking-button");
+var popup = document.querySelector(".hotel-book-window");
+var html = document.documentElement;
+
+html.className = html.className.replace("no-js", "js");
+
+//popup.classList.add("hotel-book-window-close");
+
+link.addEventListener("click", function(event) {
+    event.preventDefault();
+    if (popup.classList.contains("hotel-book-window-close")) {
+        popup.classList.add("hotel-book-window-show");
+        popup.classList.remove("hotel-book-window-close");
+    } else {
+        popup.classList.add("hotel-book-window-close");
+        popup.classList.remove("hotel-book-window-show");
+    };
+});
